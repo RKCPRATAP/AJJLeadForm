374296f79992279992a06f72ad8a4c23ae96430c
Rohit <rkcpratap@gmail.com>
2023-09-06 15:00:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function beforesubmit() {
+  let outputdate = document.querySelector(".outputdate");
+  let inputdate = document.querySelector(".inputdate");
+  console.log("inputdate.value", inputdate.value);
+
+  let formatedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+  outputdate.value = formatedDate;
+}
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
+setInterval(timestamp, 500);
